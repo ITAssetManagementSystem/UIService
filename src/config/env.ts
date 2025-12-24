d3539cb6359575253f1873d9a8acb041b2fc59f9
Nilay Saxena <nilaysaxena94@gmail.com>
2025-12-24 16:37:24 +0000

    feat: Update environment configuration and add Kubernetes manifests for deployment

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,8 +4,8 @@ const getEnv = (key: string, fallback: string) =>
     fallback;
 
 export const API = {
-    EMPLOYEE: getEnv("EMPLOYEE_API", "http://localhost:8082"),
-    ASSET: getEnv("ASSET_API", "http://localhost:8081"),
-    ASSIGNMENT: getEnv("ASSIGNMENT_API", "http://localhost:8080"),
-    REPORTING: getEnv("REPORTING_API", "http://localhost:8083")
+    EMPLOYEE: getEnv("EMPLOYEE_API", "http://employee.sikshasathi.tech"),
+    ASSET: getEnv("ASSET_API", "http://asset.sikshasathi.tech"),
+    ASSIGNMENT: getEnv("ASSIGNMENT_API", "http://assignment.sikshasathi.tech"),
+    REPORTING: getEnv("REPORTING_API", "http://reporting.sikshasathi.tech")
 };
