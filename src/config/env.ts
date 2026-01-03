13bc632e33edd457ebe6837d2e8f3127b1834a83
Nilay Saxena <nilaysaxena94@gmail.com>
2026-01-03 10:40:25 +0000

    Ingress Update

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,9 +4,9 @@ const getEnv = (key: string, fallback: string) =>
   fallback;
 
 export const API = {
-  EMPLOYEE: getEnv("EMPLOYEE_API", "http://employee.megcloud.space"),
-  ASSET: getEnv("ASSET_API", "http://asset.megcloud.space"),
-  ASSIGNMENT: getEnv("ASSIGNMENT_API", "http://assignment.megcloud.space"),
-  REPORTING: getEnv("REPORTING_API", "http://reporting.megcloud.space"),
+  EMPLOYEE: getEnv("EMPLOYEE_API", "https://employee.megcloud.space"),
+  ASSET: getEnv("ASSET_API", "https://asset.megcloud.space"),
+  ASSIGNMENT: getEnv("ASSIGNMENT_API", "https://assignment.megcloud.space"),
+  REPORTING: getEnv("REPORTING_API", "https://reporting.megcloud.space"),
 };
 
