961a894fe9915b725980e59645dd432eca4d474f
Nilay Saxena <nilaysaxena94@gmail.com>
2026-01-10 09:49:58 +0000

    Added Helm values

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,9 +4,9 @@ const getEnv = (key: string, fallback: string) =>
   fallback;
 
 export const API = {
-  EMPLOYEE: getEnv("EMPLOYEE_API", "https://employee.megcloud.space"),
-  ASSET: getEnv("ASSET_API", "https://asset.megcloud.space"),
-  ASSIGNMENT: getEnv("ASSIGNMENT_API", "https://assignment.megcloud.space"),
-  REPORTING: getEnv("REPORTING_API", "https://reporting.megcloud.space"),
+  EMPLOYEE: getEnv("EMPLOYEE_API", "https://employee.sikshasathi.tech"),
+  ASSET: getEnv("ASSET_API", "https://asset.sikshasathi.tech"),
+  ASSIGNMENT: getEnv("ASSIGNMENT_API", "https://assignment.sikshasathi.tech"),
+  REPORTING: getEnv("REPORTING_API", "https://reporting.sikshasathi.tech"),
 };
 
